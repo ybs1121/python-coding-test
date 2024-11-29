43bf246f141055617ea6fe2e216917c9b61de823
yangbyoungseok <57738933+ybs1121@users.noreply.github.com>
2024-11-30 00:53:54 +0900

    [level 3] Title: 입국심사, Time: 508.33 ms, Memory: 14.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,23 +1,21 @@
 def solution(n, times):
     answer = 0
-    start = 1
-    end = max(times)*n
-
+    end = n * max(times)
+    start = 1 
+    
     while start <= end:
-        mid = (start+end)//2
-        people = 0
+        mid = (start + end ) // 2
+        possible_total = 0
         for time in times:
-            people+= mid //time
-
-            if people >= n:
+            possible_total += (mid//time)
+            
+            if possible_total >= n:
                 break
-
-        if people>= n:
+            
+        if possible_total >= n :
             answer = mid
-            end = mid - 1
-
-        elif people < n:
-            start = mid + 1
-
-
+            end = mid-1
+        else:
+            start = mid+1
+    
     return answer
