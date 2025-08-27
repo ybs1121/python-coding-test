2422fc9215cbfb00ddd2b60f7f3def087cd5ae32
ybs1121 <ybs991@naver.com>
2025-08-27 20:02:16 +0900

    LIS/LCS

---
+++ 코딩센세/LIS_LCS/복습/LIS.py
@@ -0,0 +1,11 @@
+N = int(input())
+nums = list(map(int, input().split()))
+
+dp = [1 for _ in range(N)]
+
+for i in range(N):
+    for j in range(i):
+        if nums[i] > nums[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(max(dp))
