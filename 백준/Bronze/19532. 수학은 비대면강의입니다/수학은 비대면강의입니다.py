a85ce5c7d3bdb0e2be91297b25b6bb0cf43ba88d
yangbyoungseok <57738933+ybs1121@users.noreply.github.com>
2025-11-02 13:45:46 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 720 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -1,17 +1,11 @@
-nums = list(map(int, input().split()))
+a, b, c, d, e, f = map(int, input().split())
 
-# 2000*2000 = 4_000_000
-answer_x = -1000
-answer_y = -1000
-find = False
 for x in range(-999, 1000):
+    flag = False
     for y in range(-999, 1000):
-        if (x * nums[0] + y * nums[1]) == nums[2] and (x * nums[3] + y * nums[4]) == nums[5]:
-            answer_x = x
-            answer_y = y
-            find = True
-            break  # 답을 찾으면 바로 종료
-    if find:
+        if a * x + b * y == c and d * x + e * y == f:
+            flag = True
+            print(x, y)
+            break
+    if flag:
         break
-
-print(answer_x, answer_y)
