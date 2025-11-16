ab2acd63c9bc2d60ce0c7db5a328498fee3618c1
yangbyoungseok <57738933+ybs1121@users.noreply.github.com>
2025-11-16 11:31:13 +0900

    [Gold V] Title: 토마토, Time: 1948 ms, Memory: 50408 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -3,55 +3,60 @@ from collections import deque
 
 input = sys.stdin.readline
 
-M, N, H = map(int, input().split())
+N, M, H = map(int, input().split())
 
-multi_box = []
+maps = []
 for _ in range(H):
-    boxs = []
-    for _ in range(N):
-        boxs.append(list(map(int, input().split())))
-    multi_box.append(boxs)
+    line = []
+    for _ in range(M):
+        line.append(list(map(int, input().split())))
+    maps.append(line)
+# print(*maps, sep='\n')
 
-nx = [1, -1, 0, 0, 0, 0]
-ny = [0, 0, 1, -1, 0, 0]
-nz = [0, 0, 0, 0, -1, 1]
-
-visited = [[[False] * M for _ in range(N)] for _ in range(H)]
-
-q = deque()
-
-for z in range(H):
-    for y in range(N):
-        for x in range(M):
-            if multi_box[z][y][x] == 1:
-                q.append((x, y, z))
+dx = [1, -1, 0, 0, 0, 0]
+dy = [0, 0, 1, -1, 0, 0]
+dz = [0, 0, 0, 0, 1, -1]
 
 
-def dfs():
+def bfs():
+    flag = False
+    q = deque()
+    for z in range(H):
+        for y in range(M):
+            for x in range(N):
+                if maps[z][y][x] == 1:
+                    q.append((x, y, z))
+                if maps[z][y][x] == 0:
+                    flag = True
+    if not flag:
+        print(0)
+        return
+
     while q:
         x, y, z = q.popleft()
 
         for i in range(6):
-            dx = x + nx[i]
-            dy = y + ny[i]
-            dz = z + nz[i]
+            nx = x + dx[i]
+            ny = y + dy[i]
+            nz = z + dz[i]
 
-            if 0 <= dx < M and 0 <= dy < N and 0 <= dz < H:
-                if multi_box[dz][dy][dx] == 0:
-                    multi_box[dz][dy][dx] = multi_box[z][y][x] + 1
-                    q.append((dx, dy, dz))
+            if 0 <= nx < N and 0 <= ny < M and 0 <= nz < H:
+                # print(nx, ny, nz)
+                if maps[nz][ny][nx] == 0:
+                    maps[nz][ny][nx] = maps[z][y][x] + 1
+                    q.append((nx, ny, nz))
+
+    answer = 0
+    for z in range(H):
+        for y in range(M):
+            for x in range(N):
+                if maps[z][y][x] == 0:
+                    print(-1)
+                    return
+                answer = max(answer, maps[z][y][x])
+
+    print(answer - 1)
+    return
 
 
-dfs()
-
-answer = 0
-
-for z in range(H):
-    for y in range(N):
-        for x in range(M):
-            if multi_box[z][y][x] == 0:
-                print(-1)
-                exit(0)
-            answer = max(answer, multi_box[z][y][x])
-
-print(answer - 1)
+bfs()
