af5238840e3ce93e37260ed10efeab2ebc11c82f
yangbyoungseok <57738933+ybs1121@users.noreply.github.com>
2025-11-16 14:06:31 +0900

    [Gold IV] Title: 탈출, Time: 60 ms, Memory: 35004 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.py
+++ 백준/Gold/3055. 탈출/탈출.py
@@ -1,49 +1,62 @@
 import sys
 from collections import deque
-R, C = map(int, input().split())
-map = [list(sys.stdin.readline().rstrip()) for _ in range(R)]
 
-visited = [[-1] * C for _ in range(R)]
-dy = [-1, 1, 0, 0]
-dx = [0, 0, -1, 1]
+input = sys.stdin.readline
+
+R, C = map(int, input().split())
+
+maps = []
+for i in range(R):
+    maps.append(list(map(str, input().rstrip())))
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 q = deque()
 
-for y in range(R):
-    for x in range(C):
-        if map[y][x] == '*':  # 물 좌표를 앞에 추가
-            q.appendleft((y, x))
-        elif map[y][x] == 'S':  # 고슴이 좌표 추가
-            q.append((y, x))
-            visited[y][x] = 0  # 출발점에 시간 0 저장
+dist = []
+for i in range(R):
+    dist.append([-1] * C)
 
-while q:
-    y, x = q.popleft()
-    cur = map[y][x]  # 현재 위치
-    for i in range(4):
-        ny, nx = y + dy[i], x + dx[i]  # 다음에 갈 좌표
+for i in range(R):
+    for j in range(C):
+        if maps[i][j] == 'S':
+            q.append((i, j))
+            dist[i][j] = 0
+        elif maps[i][j] == '*':
+            q.appendleft((i, j))
 
-        if ny < 0 or ny >= R or nx < 0 or nx >= C:  # 범위 밖이면 무시
-            continue
-        if visited[ny][nx] != -1:  # 이미 방문한 곳 무시
-            continue
-        if map[ny][nx] == "*":  # 물 무시
-            continue
-        if map[ny][nx] == "X":  # 돌 무시
-            continue
-        if cur == "*" and map[ny][nx] == "D":  # 물이 비버네 가려면 무시
-            continue
+def dfs():
+    while q:
+        x, y = q.popleft()
 
-        if cur == "S":
-            if map[ny][nx] == "D":  # 고슴이가 가려는 위치가 비버네면 도착
-                print(visited[y][x] + 1)
-                break
-            visited[ny][nx] = visited[y][x] + 1  # 비버네가 아니면 도착 시간 기록
+        now = maps[x][y]
 
-        map[ny][nx] = cur  # 다음 좌표를 고슴이 or 물로 변경
-        q.append((ny, nx))  # 다음 탐색 위치 추가
-    else:
-        continue
-    break
-else:
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < R and 0 <= ny < C:
+                if dist[nx][ny] != -1:
+                    continue
+
+                if maps[nx][ny] == '*':
+                    continue
+
+                if maps[nx][ny] == 'X':
+                    continue
+
+                if now == '*' and maps[nx][ny] == 'D':
+                    continue
+
+                if now == 'S':
+                    if maps[nx][ny] == 'D':
+                        print(dist[x][y] + 1)
+                        return
+                    dist[nx][ny] = dist[x][y] + 1
+
+                maps[nx][ny] = now
+                q.append((nx, ny))
+
     print("KAKTUS")
+
+dfs()
+
