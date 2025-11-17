4a9a1388bec978af6f60ec93cbc67364eb194d5f
yangbyoungseok <57738933+ybs1121@users.noreply.github.com>
2025-11-17 20:54:02 +0900

    [Silver III] Title: 1로 만들기, Time: 596 ms, Memory: 40660 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -9,16 +9,12 @@ dp = [0 for _ in range(N + 1)]
 dp[1] = 0
 
 for i in range(2, N + 1):
+    dp[i] = dp[i - 1] + 1  # 기본값: -1 연산
+    
     if i % 2 == 0:
-        dp[i] = min(dp[i // 2] + 1, dp[i - 1] + 1)
-
+        dp[i] = min(dp[i], dp[i // 2] + 1)
+    
     if i % 3 == 0:
-        dp[i] = min(dp[i // 3] + 1, dp[i - 1] + 1)
-
-    if i % 6 == 0:
-        dp[i] = min(dp[i // 2] + 1, dp[i // 3] + 1, dp[i - 1] + 1)
-
-    if i % 2 != 0 and i % 3 != 0 and i % 6 != 0:
-        dp[i] = dp[i - 1] + 1
+        dp[i] = min(dp[i], dp[i // 3] + 1)
 
 print(dp[N])
