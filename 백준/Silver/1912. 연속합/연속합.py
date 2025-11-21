9649b20a424f1ee883c222fcb95f698543803516
yangbyoungseok <57738933+ybs1121@users.noreply.github.com>
2025-11-21 22:06:16 +0900

    [Silver II] Title: 연속합, Time: 76 ms, Memory: 39096 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,22 +1,8 @@
-import sys
-
 n = int(input())
 nums = list(map(int, input().split()))
 
-dp = [-1001] * (n + 1)
-
-dp[1] = nums[0]
-
-idx = 1
-compare_sum = nums[0]
-for i in range(2, n+1):
-
-    if 0 > compare_sum:
-        compare_sum = nums[i - 1]
-    else:
-        compare_sum += nums[i - 1]
-
-    dp[i] = max(dp[i - 1], compare_sum)
-
+dp = [0] * n
+dp[0] = nums[0]
+for i in range(1, n):
+    dp[i] = max(nums[i], dp[i-1]+nums[i])
 print(max(dp))
-# print(dp)
