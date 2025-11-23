59d5ff7aa039596c3d4d4d85496de6f17089344f
yangbyoungseok <57738933+ybs1121@users.noreply.github.com>
2025-11-23 15:56:12 +0900

    [Silver I] Title: 포도주 시식, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,14 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
 n = int(input())
-wine = [0]
+
+wine = []
 for i in range(n):
     wine.append(int(input()))
 
-dp = [0] * (n + 1)
-dp[1] = wine[1]
-if n > 1:
-    dp[2] = wine[1] + wine[2]
+dp = [0 for _ in range(n + 1)]
+dp[0] = 0
 
-for i in range(3, n + 1):
-    dp[i] = max(dp[i - 2] + wine[i], dp[i - 1], dp[i - 3] + wine[i - 1] + wine[i])
-
+for i in range(1, n + 1):
+    if i == 1:
+        dp[i] = wine[i - 1]
+    elif i == 2:
+        dp[i] = wine[i - 1] + dp[i - 1]
+    else:
+        dp[i] = max(dp[i - 1],  # 먹을 지 않았을 때
+                    wine[i - 1] + dp[i - 2],
+                    wine[i - 1] + wine[i - 2] + dp[i - 3])
 print(dp[n])
