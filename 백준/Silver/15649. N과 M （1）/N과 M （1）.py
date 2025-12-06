1df5c58667b440e176875830de7ebae4048e96c9
yangbyoungseok <57738933+ybs1121@users.noreply.github.com>
2025-12-06 11:55:46 +0900

    [Silver III] Title: N과 M (1), Time: 188 ms, Memory: 37528 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,14 +1,23 @@
-N,M = map(int,input().split())
+import sys
+input = sys.stdin.readline
 
-import itertools
-kind = []
-for i in range(N):
-    kind.append(i+1)
+N, M = map(int, input().split())
+kinds = []
+visited = [0] * (N + 1)
 
+def finder(target):
+    if len(target) == M:
+        kinds.append(target)
+        return
 
-answer = list(itertools.permutations(kind,M))
+    for num in range(1, N + 1):
+        if not visited[num]:
+            visited[num] = 1          
+            finder(target + [num])    
+            visited[num] = 0          
 
-for i in answer:
-    for j in i:
-        print(j,end=" ")
-    print()
+finder([])
+
+kinds.sort()
+for kind in kinds:
+    print(*kind)
