7b345218901737ea7cdc69dbb881f777a141bbfd
yangbyoungseok <57738933+ybs1121@users.noreply.github.com>
2025-12-14 12:11:20 +0900

    [Gold V] Title: 치킨 배달, Time: 300 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -3,13 +3,14 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
+
 maps = []
+
 for i in range(N):
     maps.append(list(map(int, input().split())))
 
-# 집과 치킨집을 분리할까
-houses = []
 chickens = []
+houses = []
 
 for i in range(N):
     for j in range(N):
@@ -18,38 +19,36 @@ for i in range(N):
         if maps[i][j] == 2:
             chickens.append([i, j])
 
-# i의 치킨집을 폐업 시킬지 말지
-visited = [0] * len(chickens)
-
 answer = sys.maxsize
 
+visited = [0 for _ in range(len(chickens))]
 
-def dfs(idx, cnt):
+
+def finder(chicken_cnt, idx):
     global N, M, answer
 
-    if cnt == M:
+    if chicken_cnt == M:
 
-        tmp_sum = 0
-        for i in range(len(houses)):
-            distance = sys.maxsize
-            for j in range(len(visited)):
-                if visited[j]:
-                    check_num = abs(houses[i][0] - chickens[j][0]) + abs(houses[i][1] - chickens[j][1])
-                    distance = min(distance, check_num)
-            tmp_sum += distance
+        summary = 0
 
-        answer = min(answer, tmp_sum)
+        for house in houses:
+            chicken_dist = sys.maxsize
+            for i in range(len(visited)):
+                if visited[i] == 1:
+                    tmp_dist = abs(chickens[i][0] - house[0]) + abs(chickens[i][1] - house[1])
+                    chicken_dist = min(chicken_dist, tmp_dist)
+            summary += chicken_dist
+
+        answer = min(answer, summary)
+
         return
 
     for i in range(idx, len(chickens)):
         if visited[i] == 0:
             visited[i] = 1
-            dfs(i + 1, cnt + 1)
+            finder(chicken_cnt + 1, i + 1)
             visited[i] = 0
 
-    return
 
-
-dfs(0, 0)
-
+finder(0, 0)
 print(answer)
