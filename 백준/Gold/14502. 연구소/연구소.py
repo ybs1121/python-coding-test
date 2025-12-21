73ebf4a9ebd9c8019c907d8e7af24c051d75c2e0
yangbyoungseok <57738933+ybs1121@users.noreply.github.com>
2025-12-21 14:44:49 +0900

    [Gold IV] Title: 연구소, Time: 1512 ms, Memory: 36060 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -7,65 +7,26 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 
 maps = []
-visited = [[False] * M for _ in range(N)]
+
 for i in range(N):
     maps.append(list(map(int, input().split())))
 
-# 북, 동, 남, 서
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-
-MAX_WALL = 3
+max_wall = 3
 
 answer = 0
 
-empty_spaces = []
+blanks = []
+virus = []
 for i in range(N):
     for j in range(M):
         if maps[i][j] == 0:
-            empty_spaces.append((i, j))
+            blanks.append([i, j])
+        if maps[i][j] == 2:
+            virus.append([i, j])
 
 
-def find(cnt):
-    global answer
-
-    if cnt == MAX_WALL:
-        # 바이러스를 경로 돌고
-        # answer 업데이트
-        tmp_map = [row[:] for row in maps]
-        answer = max(bfs(tmp_map), answer)
-        return
-
-    for walls in combinations(empty_spaces, 3):
-        tmp_maps = [row[:] for row in maps]
-
-        # 벽 세우기
-        for r, c in walls:
-            tmp_maps[r][c] = 1
-
-        answer = max(answer, bfs(tmp_maps))
-
-
-def bfs(maps):
-    q = deque()
-
-    for i in range(N):
-        for j in range(M):
-            if maps[i][j] == 2:
-                q.append((i, j))
-
-    while q:
-
-        x, y = q.popleft()
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if 0 <= nx < N and 0 <= ny < M:
-                if maps[nx][ny] == 0:
-                    q.append((nx, ny))
-                    maps[nx][ny] = 2
+def counter(maps):
+    global N, M
 
     cnt = 0
     for i in range(N):
@@ -76,6 +37,40 @@ def bfs(maps):
     return cnt
 
 
-find(0)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def finder():
+    global answer
+
+    for walls in combinations(blanks, max_wall):
+        copy_map = []
+
+        for i in range(N):
+            copy_map.append(maps[i] + [])
+
+        for wall in walls:
+            copy_map[wall[0]][wall[1]] = 1
+
+        # 전이
+        q = deque(virus)
+
+        while q:
+            x, y = q.popleft()
+
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+
+                if 0 <= nx < N and 0 <= ny < M:
+                    if copy_map[nx][ny] == 0:
+                        copy_map[nx][ny] = 2
+                        q.append((nx, ny))
+
+        answer = max(counter(copy_map), answer)
+
+
+finder()
 
 print(answer)
